338f4892829b22622956163596d7c04c217a1dbc
cl7zn <67072900+cl7zn@users.noreply.github.com>
2023-04-02 22:43:53 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,1 +1,32 @@
+import shared as sh
+sh.afunction()
 
+def test_multi_spaces():
+    assert sh.space_compress('word       word     word') == 'word word word', "Failed multi space"
+
+
+def test_multi_lines():
+    multiline = """
+    word
+    word
+    word
+    """
+    assert sh.space_compress(multiline) == 'word word word', "Failed multi line"
+
+
+def test_assert_int():
+    with pytest.raises(Exception) as exc_mssg:
+        sh.space_compress(2)
+
+    assert "Expected str but got" in str(exc_mssg)
+    assert "int" in str(exc_mssg)
+
+
+def test_assert_bool():
+    with pytest.raises(Exception) as exc_mssg:
+        sh.space_compress(False)
+
+    assert "Expected str but got" in str(exc_mssg)
+    assert "bool" in str(exc_mssg)
+
+test_assert_int(7)
